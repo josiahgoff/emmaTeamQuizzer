4a3a9c309ebf0ce97f09bf58e21f362f124c3bf9
Josiah Goff <josiahgoff@gmail.com>
2016-01-24 17:10:19 -0600

    Fade incorrect answers

--- client/views/quiz/quizResults.js
+++ client/views/quiz/quizResults.js
@@ -14,6 +14,13 @@ Template['quizResults'].helpers({
     console.log(this);
     return parent.solution._id === parent.answerSubmitted ?
       parent.solution.photo2 : this.photo;
+  },
+
+  answerClasses: function() {
+    var data = Template.instance().data;
+    console.log(this);
+    return this.solution._id !== this.answerSubmitted ?
+      ' incorrect' : '';
   }
 });
 
